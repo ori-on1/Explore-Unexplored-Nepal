6eb777a573543883f656fa222c8b09cdace234d4
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2024-11-11 20:09:22 +0000

    fix search issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import ProductList from "../components/ProductList";
 import exploreNepal from "../data/exploreNepalData";
-import { useState } from "react";
-const Home = () => {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [searchText, setSearchText] = useState("");
-  const categories = [...new Set(exploreNepal.map((location) => location.category))];
+
+const Home = ({ searchText, selectedCategory }) => {
   const filteredLocations = exploreNepal.filter((location) => {
     const matchesCategory = selectedCategory ? location.category === selectedCategory : true;
-    const matchesSearch = location.name.toLowerCase().includes(searchText.toLowerCase());
+    const matchesSearch = searchText ? location.name.toLowerCase().includes(searchText.toLowerCase()) : true;
     return matchesCategory && matchesSearch;
   });
+
   return (
-       <main className="container mx-auto px-4 py-6">
+    <main className="container mx-auto px-4 py-6">
       <ProductList locations={filteredLocations} />
     </main>
-  )
+  );
 };
 
 export default Home;
